271f938bb18d5127ec0239c80549c5e00dc6188b
Marie313 <marie.zielke@telekom.de>
2023-11-17 11:04:28 +0100

    new

--- edit.js
+++ edit.js
@@ -1,18 +1,133 @@
-function nameFunction() {
-    let name = prompt("Please enter the new name", "new name");
-    if (name != null) {
-      document.getElementById("name").innerHTML =
-      "name: " + name ;
+// Laden der ausgewählten Variable-ID aus der URL
+const urlParams = new URLSearchParams(window.location.search);
+const selectedVariableId = urlParams.get('id');
+
+// Alle Jobs in einem Array
+const allJobs = [
+    {
+        id: 1234,
+        name: "Beispiel_1",
+        enabled: true,
+        status: "success",
+        firstRun: "2023-10-12T11:24",
+        nextRun: "2023-10-13T11:24",
+        lastRun: "2023-10-12T11:24",
+        interval: "every_86.400_seconds"
+    },
+    {
+        id: 2023,
+        name: "Beispie_2",
+        enabled: true,
+        status: "success",
+        firstRun: "2023-06-14T22:24",
+        nextRun: "2023-12-14T22:24",
+        lastRun: "2023-06-14T22:24",
+        interval: "every_15.768.000_seconds"
+    },
+    {
+        id: 5678,
+        name: "Beispie_3",
+        enabled: true,
+        status: "warning",
+        firstRun: "2023-09-12T10:19",
+        nextRun: "2023-11-12T10:15",
+        lastRun: "2023-09-12T10:19",
+        interval: "every_7.884.000_seconds"
+    },
+    {
+        id: 1010,
+        name: "Beispiel_4",
+        enabled: false,
+        status: "none",
+        firstRun: "",
+        nextRun: "",
+        lastRun: "",
+        interval: ""
+    },
+    {
+        id: 3103,
+        name: "Beispiel_5",
+        enabled: true,
+        status: "failed",
+        firstRun: "2023-10-08T09:35",
+        nextRun: "2023-10-18T09:35",
+        lastRun: "2023-10-08T09:35",
+        interval: "every_864.000_seconds"
+    },
+    {
+        id: 9876,
+        name: "Beispiel_6",
+        enabled: true,
+        status: "success",
+        firstRun: "2023-10-12T10:01",
+        nextRun: "2023-10-13T10:02",
+        lastRun: "2023-10-12T10:01",
+        interval: "every_60_seconds"
+    },
+    {
+        id: 2005,
+        name: "Beispiel_7",
+        enabled: false,
+        status: "success",
+        firstRun: "2015-12-30T04:24",
+        nextRun: "",
+        lastRun: "2015-12-30T04:24",
+        interval: ""
     }
-  }
-function runFunction() {
-    let run = prompt("Please enter the time interval at which the system should run", "new time interval");
-    if (run != null) {
-      document.getElementById("run").innerHTML =
-      "run: " + run ;
+];
+
+// Überprüfen, ob eine Variable ausgewählt wurde
+if (selectedVariableId) {
+    // Anzeigen der ausgewählten Variable auf der Seite
+    const jobDetails = document.getElementById('jobDetails');
+
+    // Wählen Sie die entsprechende Variable basierend auf der ausgewählten ID
+    const selectedJob = allJobs.find(job => job.id === parseInt(selectedVariableId));
+
+    if (selectedJob) {
+        const checkboxHtml = `
+            <p>Enabled:</p>
+            <div class="checkbox">
+                <input type="checkbox" class="my-checkbox" ${selectedJob.enabled ? 'checked' : ''}>
+            </div>
+            <div class="message"><p class="message">${selectedJob.enabled}</p></div>
+        `;
+        // Anzeigen der Details der ausgewählten Variable auf der Seite
+        jobDetails.innerHTML = `
+            <p>ID: ${selectedJob.id}</p>
+            <label>Name: </label><input value=${selectedJob.name}>
+            ${checkboxHtml}
+            <p>Status: ${selectedJob.status}</p>
+            <label>First Run: </label><input type="datetime-local" value=${selectedJob.firstRun}>
+            <label>Next Run: </label><input type="datetime-local" value=${selectedJob.nextRun}>
+            <label>Last Run: </label><input type="datetime-local" value=${selectedJob.lastRun}>
+            <label>Interval: </label> <input value=${selectedJob.interval}>
+        `;
+    } else {
+        console.error('Ungültige Variable-ID');
     }
+} else {
+    // Falls keine Variable ausgewählt wurde, eine Meldung anzeigen oder andere Aktionen durchführen
+    console.error('Keine Variable ausgewählt');
 }
-function showInput(){
-  
-}
-  
+// Erstelle ein link-Element
+var linkElement = document.createElement('link');
+
+// Setze die Attribute des link-Elements
+linkElement.rel = 'stylesheet';
+linkElement.type = 'text/css';
+linkElement.href = 'edit.css';
+
+// Füge das link-Element dem head-Element der Seite hinzu
+document.head.appendChild(linkElement);
+
+var checkbox = document.querySelectorAll(".my-checkbox");
+var message = document.querySelectorAll(".message");
+checkbox.forEach(function(checkbox, index){
+    checkbox.addEventListener('change',function(){
+        if (checkbox.checked){
+            message[index].textContent='true'}
+        else {
+            message[index].textContent='false'}
+    });
+});
--- index.js
+++ index.js
@@ -1,7 +1,114 @@
-function isChecked(){
-    if(document.getElementById("my-checkbox").checked){
-    document.getElementById("message").textContent = "Enabled";}
-    else{
-    document.getElementById("message").textContent = "Disabled";
+var checkbox = document.querySelectorAll(".my-checkbox");
+var message = document.querySelectorAll(".message");
+checkbox.forEach(function(checkbox, index){
+    checkbox.addEventListener('change',function(){
+        if (checkbox.checked){
+            message[index].textContent='Enabled'}
+        else {
+            message[index].textContent='Disabled'}
+    });
+});
+// Laden der ausgewählten Variable-ID aus der URL
+const urlParams = new URLSearchParams(window.location.search);
+const selectedVariableId = urlParams.get('id');
+
+// Alle Jobs in einem Array
+const allJobs = [
+    {
+        id: 1234,
+        name: "Beispiel_1",
+        enabled: true,
+        lastRun: "2023-10-12T11:24",
+        nextRun: "2023-10-13T11:24",
+        backgroundColor: "rgba(61, 255, 61, 0.75)"
+    },
+    {
+        id: 2023,
+        name: "Beispie_2",
+        enabled: true,
+        lastRun: "2023-06-14T22:24",
+        nextRun: "2023-12-14T22:24",
+        backgroundColor: "rgba(61, 255, 61, 0.75)"
+    },
+    {
+        id: 5678,
+        name: "Beispie_3",
+        enabled: true,
+        lastRun: "2023-09-12T10:19",
+        nextRun: "2023-11-12T10:15",
+        backgroundColor: "rgb(255, 252, 71, 0.8)"
+    },
+    {
+        id: 1010,
+        name: "Beispiel_4",
+        enabled: false,
+        lastRun: "-",
+        nextRun: "-",
+    },
+    {
+        id: 3103,
+        name: "Beispiel_5",
+        enabled: true,
+        lastRun: "2023-10-08T09:35",
+        nextRun: "2023-10-18T09:35",
+        backgroundColor: "rgba(255, 66, 66, 0.726)"
+    },
+    {
+        id: 9876,
+        name: "Beispiel_6",
+        enabled: true,
+        lastRun: "2023-10-12T10:01",
+        nextRun: "2023-10-13T10:02",
+        backgroundColor: "rgb(61,255, 61, 0.75)"
+    },
+    {
+        id: 2005,
+        name: "Beispiel_7",
+        enabled: false,
+        lastRun: "2015-12-30T04:24",
+        nextRun: "-",
+        backgroundColor: "rgba(61, 255, 61, 0.75)"
     }
-}
+];
+// Anzeigen der Jobs in der Tabelle
+const jobDetailsTableBody = document.getElementById('jobTable');
+
+    allJobs.forEach(job => {
+    const jobDetailRow = document.createElement('tr');
+    jobDetailRow.className = 'job-detail';
+
+    const jobDetailCells = [
+        { value: job.id},
+        { value: job.name},
+        { 
+            value: `<input type="checkbox" class="enabled-checkbox" ${job.enabled ? 'checked' : ''} disabled>`
+        },
+        { value: job.lastRun },
+        { value: job.nextRun },
+        { 
+            value: `
+                    <div class="edit"><a href="edit.html?id=${job.id}"><svg width="16" height="16"><use xlink:href="#edit-icon"></use></svg></a></div>
+                    <div class="delete"><button onClick="alert('really want to delete this process?')" class="deleteButton"><svg width="16" height="16"><use xlink:href="#delete-icon"></use></svg></button></div>
+            `,
+        },
+    ];
+
+    jobDetailCells.forEach(cell => {
+        const tdElement = document.createElement('td');
+        tdElement.className = cell.className || '';
+        tdElement.innerHTML = cell.value;
+        jobDetailRow.appendChild(tdElement);
+    });
+
+    jobDetailRow.style.backgroundColor = job.backgroundColor || '';
+
+    jobDetailsTableBody.appendChild(jobDetailRow);
+});
+
+var deleteButton = document.querySelectorAll('.deleteButton')
+deleteButton.forEach(function(button){
+    button.addEventListener('click',function(){
+        var row = button.closest('tr');
+        row.remove();
+    });
+});
